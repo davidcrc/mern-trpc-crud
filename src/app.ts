b6c55b9678ae2516891d0e59cf9c8076197f2015
davidcrc <david_drcr@hotmail.com>
2023-06-26 11:46:49 -0500

    feat: :fire: trpc setup

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,22 @@
 import dotenv from "dotenv";
 import express from "express";
 import morgan from "morgan";
+import * as trpcExpress from "@trpc/server/adapters/express";
+import { createContext, router } from "./server/trpc";
 
 dotenv.config();
 
 const app = express();
 app.use(morgan("dev"));
+
+const appRouter = router({});
+
+app.use(
+  "/trpc",
+  trpcExpress.createExpressMiddleware({
+    router: appRouter,
+    createContext, //for authentication
+  })
+);
 
 export default app;
