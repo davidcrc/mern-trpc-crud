009dd955c97eb638d058bb782aca71fdf5d6a35d
davidcrc <david_drcr@hotmail.com>
2023-06-26 16:38:29 -0500

    feat: delete card

--- src/routes/notes.ts
+++ src/routes/notes.ts
@@ -12,7 +12,6 @@ const createNoteType = { title: z.string(), description: z.string() };
 const createNote = publicProcedure
   .input(z.object(createNoteType))
   .mutation(async ({ input }) => {
-
     // TODO: maybe add try catch
     const note = await prisma.notes.create({
       data: {
@@ -21,11 +20,26 @@ const createNote = publicProcedure
       },
     });
 
-
     return note;
+  });
+
+const deleteNode = publicProcedure
+  .input(z.string())
+  .mutation(async ({ input }) => {
+
+    const note = await prisma.notes.delete({
+      where: {
+        uuid: input,
+      },
+    });
+
+    if (!note) throw new Error("Note not found");
+
+    return true;
   });
 
 export const notesRouter = router({
   get: getNotes,
   create: createNote,
+  delete: deleteNode,
 });
